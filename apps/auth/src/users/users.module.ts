82717b7f276bb787cf2b08249ac955c0ae94cbfb
MilitsaTsvetkova <militsa88@yahoo.com>
2025-01-20 10:18:16 +0200

    productionize

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,6 +1,9 @@
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
-import { UserDocument, userSchema } from './models/user.schema';
+import {
+  UserDocument,
+  userSchema,
+} from '../../../../libs/common/src/models/user.schema';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
