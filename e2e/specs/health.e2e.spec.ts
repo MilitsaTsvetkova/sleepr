aeef755d45212b7fc80f3e16766e262ee779411b
MilitsaTsvetkova <militsa88@yahoo.com>
2025-02-01 19:54:37 +0200

    e2e create and get reservation

--- e2e/specs/health.e2e.spec.ts
+++ e2e/specs/health.e2e.spec.ts
@@ -1,40 +1,31 @@
 import { ping } from 'tcp-ping';
 
-describe('Health E2E', () => {
-  it('Reservations', async () => {
+describe('Health', () => {
+  test('Reservations', async () => {
     const response = await fetch('http://reservations:3000');
     expect(response.ok).toBeTruthy();
   });
-  it('Auth', async () => {
-    const res = await fetch('http://auth:3001');
-    expect(res.ok).toBeTruthy();
+
+  test('Auth', async () => {
+    const response = await fetch('http://auth:3001');
+    expect(response.ok).toBeTruthy();
   });
-  it('Payments', (done) => {
-    ping(
-      {
-        address: 'payments',
-        port: 3003,
-      },
-      (err) => {
-        if (err) {
-          fail();
-        }
-        done();
-      },
-    );
+
+  test('Payments', (done) => {
+    ping({ address: 'payments', port: 3003 }, (err) => {
+      if (err) {
+        fail();
+      }
+      done();
+    });
   });
-  it('Notifications', (done) => {
-    ping(
-      {
-        address: 'notifications',
-        port: 3004,
-      },
-      (err) => {
-        if (err) {
-          fail();
-        }
-        done();
-      },
-    );
+
+  test('Notifications', (done) => {
+    ping({ address: 'notifications', port: 3004 }, (err) => {
+      if (err) {
+        fail();
+      }
+      done();
+    });
   });
 });
--- e2e/specs/reservations.spec.ts
+++ e2e/specs/reservations.spec.ts
@@ -1,16 +1,69 @@
 describe('Reservations E2E', () => {
+  let jwt: string;
   beforeAll(async () => {
     const user = {
-      emailAddress: '',
-      password: 'StrongPassword1!',
+      email: 'sleeprnestapp@gmail.com',
+      password: 'StrogPassword123!@',
     };
-
-    await fetch('http://auth:3001', {
+    await fetch('http://auth:3001/users', {
       method: 'POST',
+      body: JSON.stringify(user),
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(user),
     });
+    const response = await fetch('http://auth:3001/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    jwt = await response.text();
   });
+
+  test('Create and Get a reservation', async () => {
+    const createdReservation = await createReservation();
+    const responseGet = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          Authentication: jwt,
+        },
+      },
+    );
+
+    const reservation = await responseGet.json();
+    expect(createdReservation).toEqual(reservation);
+  });
+
+  const createReservation = async () => {
+    const reservation = {
+      startDate: '12-25-2022',
+      endDate: '12-25-2023',
+      placeId: '123',
+      invoiceId: '123',
+      charge: {
+        amount: 17,
+        card: {
+          cvc: '123',
+          exp_month: 12,
+          exp_year: 2026,
+          number: '4242 4242 4242 4242',
+        },
+      },
+    };
+
+    const response = await fetch('http://reservations:3000/reservations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authentication: jwt,
+      },
+      body: JSON.stringify(reservation),
+    });
+
+    expect(response.status).toBe(201);
+    return response.json();
+  };
 });
