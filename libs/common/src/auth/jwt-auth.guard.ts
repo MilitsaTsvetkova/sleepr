b681582cf43567aee31ce17daaeae2267ed300a0
MilitsaTsvetkova <militsa88@yahoo.com>
2025-02-16 16:00:43 +0200

    rework dto/auth guard to use newly defined types

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -4,21 +4,26 @@ import {
   Inject,
   Injectable,
   Logger,
+  OnModuleInit,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientGrpc } from '@nestjs/microservices';
 import { catchError, map, Observable, of, tap } from 'rxjs';
-import { AUTH_SERVICE } from '../constants';
-import { UserDto } from '../dto';
+import { AUTH_SERVICE_NAME, AuthServiceClient } from '../types';
 
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
+export class JwtAuthGuard implements CanActivate, OnModuleInit {
   private readonly logger: Logger = new Logger(JwtAuthGuard.name);
+  private authService: AuthServiceClient;
   constructor(
-    @Inject(AUTH_SERVICE) private readonly authClient: ClientProxy,
+    @Inject(AUTH_SERVICE_NAME) private readonly client: ClientGrpc,
     private readonly reflector: Reflector,
   ) {}
+  onModuleInit() {
+    this.authService =
+      this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
@@ -30,25 +35,26 @@ export class JwtAuthGuard implements CanActivate {
     }
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
 
-    return this.authClient
-      .send<UserDto>('authenticate', { Authentication: jwt })
-      .pipe(
-        tap((response) => {
-          if (roles) {
-            for (const role of roles) {
-              if (!response.roles.includes(role)) {
-                this.logger.error('The user does not have valid roles');
-                throw new UnauthorizedException();
-              }
+    return this.authService.authenticate({ Authentication: jwt }).pipe(
+      tap((response) => {
+        if (roles) {
+          for (const role of roles) {
+            if (!response.roles.includes(role)) {
+              this.logger.error('The user does not have valid roles');
+              throw new UnauthorizedException();
             }
           }
-          context.switchToHttp().getRequest().user = response;
-        }),
-        map(() => true),
-        catchError((err) => {
-          this.logger.error(err);
-          return of(false);
-        }),
-      );
+        }
+        context.switchToHttp().getRequest().user = {
+          ...response,
+          _id: response.id,
+        };
+      }),
+      map(() => true),
+      catchError((err) => {
+        this.logger.error(err);
+        return of(false);
+      }),
+    );
   }
 }
