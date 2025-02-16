9920c81da562f62f4ca8dda25d316509a57ad134
MilitsaTsvetkova <militsa88@yahoo.com>
2025-02-16 16:50:04 +0200

    rework auth service to use gRPC

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,9 +1,11 @@
+import { AUTH_PACKAGE_NAME } from '@app/common';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import * as cookieParser from 'cookie-parser';
 import { Logger } from 'nestjs-pino';
+import { join } from 'path';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
@@ -11,10 +13,11 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.connectMicroservice({
-    transport: Transport.TCP,
+    transport: Transport.GRPC,
     options: {
-      host: '0.0.0.0',
-      port: configService.get('TCP_PORT'),
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, '../../../proto/auth.proto'),
+      url: configService.getOrThrow<string>('AUTH_GRPC_URL'),
     },
   });
   app.use(cookieParser());
