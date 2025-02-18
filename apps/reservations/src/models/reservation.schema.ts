34ac3998510d86d07c417f34ab1327fd1b9aa45a
MilitsaTsvetkova <militsa88@yahoo.com>
2025-02-18 10:08:22 +0200

    added reservations resolver

--- apps/reservations/src/models/reservation.schema.ts
+++ apps/reservations/src/models/reservation.schema.ts
@@ -1,19 +1,26 @@
 import { AbstractDocument } from '@app/common';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({
   versionKey: false,
 })
+@ObjectType()
 export class ReservationDocument extends AbstractDocument {
   @Prop()
+  @Field()
   timeStamp: Date;
   @Prop()
+  @Field()
   startDate: Date;
   @Prop()
+  @Field()
   endDate: Date;
   @Prop()
+  @Field()
   userId: string;
   @Prop()
+  @Field()
   invoiceId: string;
 }
 
